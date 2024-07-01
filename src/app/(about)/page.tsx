899fba095a023df1c34d0296f503b891d0d8a25f
Ryan Apriansyah <85532633+raynapriansyah@users.noreply.github.com>
2024-07-01 12:25:46 +0700

    second

--- src/app/(about)/page.tsx
+++ src/app/(about)/page.tsx
@@ -13,8 +13,8 @@ export default function AboutPage() {
       </section>
       <section className="text-center text-xl">
         <h1>
-          Frontend developer committed to crafting responsive and user friendly
-          web applications.
+          Frontenddd developer committed to crafting responsive and user
+          friendly web applications.
         </h1>
       </section>
       <div className="gap-x-8 flex items-center justify-center">
