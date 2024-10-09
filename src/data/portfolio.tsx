3076b81ecae99ad835df8f8a3d310f45437667f6
raynapriansyah <ryan.apriansyah46@gmail.com>
2024-10-09 11:48:13 +0700

    add 2 portofolio

--- src/data/portfolio.tsx
+++ src/data/portfolio.tsx
@@ -3,10 +3,63 @@ import Link from "next/link";
 import Thumbnail_1 from "/public/image/thumbnail-01.png";
 import Thumbnail_2 from "/public/image/thumbnail-02.png";
 import Thumbnail_3 from "/public/image/thumbnail-03.png";
+import Thumbnail_4 from "/public/image/thumbnail-04.png";
+import Thumbnail_5 from "/public/image/thumbnail-05.png";
 import { Eye, Github } from "lucide-react";
 import { buttonVariants } from "~/components/ui/button";
 
 export const portfolio = [
+  {
+    title: "Product",
+    image: (
+      <Image className="w-full rounded-sm" src={Thumbnail_4} alt="thumbnail" />
+    ),
+
+    description:
+      "Build Project using React Typescript, Redux, Axios and implemented product-api - Fullstack Developer Test",
+    code: (
+      <Link
+        href="https://github.com/raynapriansyah/product"
+        className={buttonVariants({ className: "rounded-2xl" })}
+      >
+        <section className="flex items-center gap-2">
+          <Github /> Code
+        </section>
+      </Link>
+    ),
+    demo: (
+      <Link href="#" className={buttonVariants({ className: "rounded-2xl" })}>
+        <section className="flex items-center gap-2">
+          <Eye /> Preview
+        </section>
+      </Link>
+    ),
+  },
+  {
+    title: "Product-api",
+    image: (
+      <Image className="w-full rounded-sm" src={Thumbnail_5} alt="thumbnail" />
+    ),
+
+    description: "Build Rest API using Laravel 11 - Fullstack Developer Test",
+    code: (
+      <Link
+        href="https://github.com/raynapriansyah/product-api"
+        className={buttonVariants({ className: "rounded-2xl" })}
+      >
+        <section className="flex items-center gap-2">
+          <Github /> Code
+        </section>
+      </Link>
+    ),
+    demo: (
+      <Link href="#" className={buttonVariants({ className: "rounded-2xl" })}>
+        <section className="flex items-center gap-2">
+          <Eye /> Preview
+        </section>
+      </Link>
+    ),
+  },
   {
     title: "Luke Skywalker",
     image: (
